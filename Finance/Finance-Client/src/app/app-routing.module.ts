ee5c8106414c1f7e730bcbb70d54ca0c5a4c84b7
pavithradarani17 <pavithradarani17@gmail.com>
2021-10-19 21:48:54 -0700

    added user profile

--- Finance/Finance-Client/src/app/app-routing.module.ts
+++ Finance/Finance-Client/src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { BuyProductComponent } from './buy-product/buy-product.component';
 import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
-
+import { CarddetailsComponent } from './carddetails/carddetails.component';
 
 const routes: Routes = [
   {path:'admin',component:AdminComponent},
   {path:'product',component:ProductDashboardComponent},
-  {path:'buyproduct/:product_id',component:BuyProductComponent}
-
+  {path:'buyproduct/:product_id',component:BuyProductComponent},
+  {path:'profile',component:CarddetailsComponent},
+  
 ];
 
 @NgModule({
