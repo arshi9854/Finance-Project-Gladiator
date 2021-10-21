24b0327c9350138a1c425e195629c5e01c67c7f3
adnan <adnananjum12@gmail.com>
2021-10-21 10:21:28 +0530

    Alerts

--- Finance/Finance-Client/src/app/user-activation/user-activation.component.ts
+++ Finance/Finance-Client/src/app/user-activation/user-activation.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 import { UserDetailsService } from '../services/user-details.service';
 import { allUsers } from '../shared/user-details';
 
@@ -10,46 +11,68 @@ import { allUsers } from '../shared/user-details';
   styleUrls: ['./user-activation.component.css']
 })
 export class UserActivationComponent implements OnInit {
-  userList! : allUsers[];
-  userData! : allUsers[];
-  activated! : boolean;
+  userList!: allUsers[];
+  userData!: allUsers[];
+  activated!: boolean;
   constructor(
-    private detailService:UserDetailsService,
+    private detailService: UserDetailsService,
     private router: Router
   ) { }
 
   ngOnInit(): void {
-    this.detailService.getProducts().subscribe((data)=>{
-      this.userList=data;
-      this.userData=this.userList;
+    this.detailService.getProducts().subscribe((data) => {
+      this.userList = data;
+      this.userData = this.userList;
     })
   }
 
-  handle_userid_activation=(userId: number,cardType: string)=>{
+  handle_userid_activation = (userId: number, cardType: string) => {
     console.log(userId)
-    if(confirm("Are you sure you want to ACTIVATE user?")){
-      this.detailService.user_activation(userId).subscribe();
-      if(cardType==="Gold"){
-      this.detailService.card_activation(userId,1).subscribe();
+    Swal.fire({
+      title: 'Do you want to activate the user?',
+      showDenyButton: true,
+      // showCancelButton: true,
+      confirmButtonText: 'Activate',
+      denyButtonText: `Don't Activate`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.detailService.user_activation(userId).subscribe();
+        if (cardType === "Gold") {
+          this.detailService.card_activation(userId, 1).subscribe();
+        }
+        else {
+          this.detailService.card_activation(userId, 2).subscribe();
+        }
+        Swal.fire('User ' + userId, 'Activated', 'success').then(function () {
+          location.reload();
+        })
+      } else if (result.isDenied) {
+        Swal.fire('User was not Activated', '', 'info').then(function () {
+          location.reload();
+        })
       }
-      else{
-        this.detailService.card_activation(userId,2).subscribe();
-      }
-      window.location.reload();
-    }
-    else{
-      window.location.reload();
-    }
+    })
   }
 
-  handle_userid_deletion(userId: number){
+  handle_userid_deletion(userId: number) {
     console.log(userId)
-    if(confirm("Are you sure you want to DELETE user?")){
-    this.detailService.user_deletion(userId).subscribe();
-    window.location.reload();
-    }
-    else{
-    window.location.reload();
-    }
+
+    Swal.fire({
+      title: 'Do you want to delete the user?',
+      showCancelButton: true,
+      confirmButtonText: 'Delete',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.detailService.user_deletion(userId).subscribe();
+        Swal.fire('User ' + userId, 'Deleted', 'success').then(function () {
+          location.reload();
+        })
+      } else if (result.isDenied) {
+        Swal.fire('Deletion of User Cancelled', '', 'info').then(function () {
+          location.reload();
+        })
+      }
+    })
+
   }
 }
